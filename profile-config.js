4ed36b7cf7037ed2de1cb20191e56e9bdf9658fa
supattarin22-sketch <supattarin22@gmail.com>
2026-09-03 15:43:06 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbzZPKCjqrvptWM7nVyquVLeU2GlKrS2UtljX1vQCqDHR2UVsL_-Tyh5BqVctKUwsv1C/exec';
+  'https://script.google.com/macros/s/AKfycbw5jah0GkRFlqHSYM9BRaTvoDnZ9vXZKsKHvlNKoZW7tI0VyetRGuYjKoC8LL-PcSk/exec';
