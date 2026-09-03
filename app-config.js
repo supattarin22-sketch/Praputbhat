a91cd2eb1ffaeb4592e7c44bda7c4840d0e24597
supattarin22-sketch <supattarin22@gmail.com>
2026-09-03 15:48:25 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbxba2Enfzaz4wu7supLpkW2V-3aQLk4AG6KtPjc6GczcifG4J7wXpcE85pmAZsoKXdBhg/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbzp5lKLVpQ5eTSJhjko8rs-ggHEYorLFJiOzyti0Vtv2CALNp-yr98ApxEbsVSYBDg/exec'
   });
 })();
